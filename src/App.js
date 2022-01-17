0c84233aafa89949a6a3e66e9787925007558f85
Deepak Mane <deepak.mane123@gmail.com>
2022-01-17 12:23:05 -0800

    Services Completed

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Particles from 'react-tsparticles';
 import Navbar from './components/Navbar';
 import Header from './components/Header';
 import AboutMe from './components/AboutMe';
+import Services from './components/Services';
 
 function App() {
   const particlesInit = (main) => {
@@ -96,6 +97,7 @@ function App() {
       <Navbar />
       <Header />
       <AboutMe />
+      <Services/>
     </>
   );
 }
